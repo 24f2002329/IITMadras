8dfef023dbd809107a8e57a273de1cbbc0ec8ca0
devendra-sjt <devendrachouhan.sjt@gmail.com>
2025-01-01 13:40:33 +0530

    Created venv in Web App Folder

--- Web App/Quiz_Master_v1/__init__.py
+++ Web App/Quiz_Master_v1/__init__.py
@@ -1,1 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+def create_app():
+    app = Flask(__name__)
+    return app
