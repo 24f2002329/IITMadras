d60f28cf32734541af5316c502ee68fb470089e4
devendra-sjt <devendrachouhan.sjt@gmail.com>
2025-01-01 18:25:36 +0530

    updated

--- Web App/Quiz_Master_v1/__init__.py
+++ Web App/Quiz_Master_v1/__init__.py
@@ -8,4 +8,15 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'mysecretkey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    with app.app_context():
+        db.create_all()
+        print('Created Database!')
---
+++ Web App/Quiz_Master_v1/models.py
@@ -0,0 +1,13 @@
+from . import db   # here '.' means the __init__.py file in the same directory
+from flask_login import UserMixin
+from datetime import datetime
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(50))
+    quizzes = db.relationship('Quiz', backref='author', lazy=True)
+    created_date = db.Column(db.DateTime, default=datetime.utcnow().date())
+    updated_date = db.Column(db.DateTime, default=datetime.utcnow().date())
+
